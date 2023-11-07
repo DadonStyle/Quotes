521b89690e1a6403e45afe4e0a410a8a94dfe199
Noam <noamoni9@gmail.com>
2023-11-07 09:52:38 +0200

    add prac

--- BE/test.js
+++ BE/test.js
@@ -57,28 +57,68 @@
 // console.log(sumZero([-3, -2, -1, 1]));
 
 // sort by color than id
-const data = [
-  { color: 'yellow', id: 2 },
-  { color: 'black', id: 3 },
-  { color: 'yellow', id: 5 },
-  { color: 'red', id: 1 },
-  { color: 'red', id: 4 },
-];
+// const data = [
+//   { color: 'yellow', id: 2 },
+//   { color: 'black', id: 3 },
+//   { color: 'yellow', id: 5 },
+//   { color: 'red', id: 1 },
+//   { color: 'red', id: 4 },
+// ];
 
-const sort = () => {
-  const mapByColors = new Map();
-  for (let i = 0; i < data.length; i++) {
-    console.log(mapByColors.get(data[i].color));
-    if (mapByColors.has(data[i].color)) {
-      mapByColors.set(data[i].color, [
-        ...mapByColors.get(data[i].color),
-        data[i],
-      ]);
-    } else {
-      mapByColors.set(data[i].color, [data[i]]);
-    }
-  }
-  console.log(mapByColors);
+// const sort = () => {
+//   const mapByColors = new Map();
+//   for (let i = 0; i < data.length; i++) {
+//     // console.log(mapByColors.get(data[i].color));
+//     if (mapByColors.has(data[i].color)) {
+//       mapByColors.set(data[i].color, [
+//         ...mapByColors.get(data[i].color),
+//         data[i],
+//       ]);
+//     } else {
+//       mapByColors.set(data[i].color, [data[i]]);
+//     }
+//   }
+//   const res = [];
+//   for (let [, value] of mapByColors) {
+//     const sorted = value.sort((a, b) => {
+//       if (a.id < b.id) return -1;
+//       if (a.id > b.id) return 1;
+//     });
+//     res.push(...sorted);
+//   }
+//   console.log(res);
+// };
+
+// sort();
+
+// const reverse = (s) => {
+//   if (s.length < 1) return s;
+//   return s.at(-1) + reverse(s.slice(0, s.length - 1));
+// };
+// console.log(reverse('noam'));
+
+// String.prototype.isPalindrom = function () {
+//   let rev = this.split('').reverse().join('');
+//   if (this.toString() === rev) return true;
+//   return false;
+// };
+
+// console.log('abba'.isPalindrom());
+
+// Array.prototype.myMap = (cb) => {
+//   const resArr = [];
+//   for (let i = 0; i < this.length; i++) {
+//     resArr.push(cb(this[i]));
+//   }
+//   return resArr;
+// };
+
+const isObj = (random) => {
+  if (typeof random === 'object' && random !== null && !Array.isArray(random))
+    return true;
+  return false;
 };
 
-sort();
+console.log(isObj([]));
+console.log(isObj('s'));
+console.log(isObj({ key: 'noam', val: 'val' }));
